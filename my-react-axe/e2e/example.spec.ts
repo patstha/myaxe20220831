238ad4a8dfe9629e8966451ac21121cddbf75981
Pat <pat@tilde.team>
2022-09-01 04:19:06 -0500

    add playwright on react

---
+++ my-react-axe/e2e/example.spec.ts
@@ -0,0 +1,11 @@
+// import { test, expect } from '@playwright/test';
+
+// test('check page title and url port number test', async ({ page }) => {
+//   await page.goto('/');
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/MyAngularAxe/);
+
+//   // Expects the URL to contain intro.
+//   await expect(page).toHaveURL(/.*4200/);
+// });
